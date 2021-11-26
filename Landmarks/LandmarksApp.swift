d6a7b0df760f47653d6f7e52179cb6340de2bace
OpsecOdyssey <ferry.keegan@gmail.com>
2021-11-26 14:52:48 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Keegan Ferry on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
